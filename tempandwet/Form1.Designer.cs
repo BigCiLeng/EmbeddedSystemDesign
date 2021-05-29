6fc38390c6dc26f0b54945a76514fb23516d482d
BigCileng <w15516937650@outlook.com>
2021-05-29 17:13:35 +0800

    添加项目文件。

---
+++ tempandwet/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿
+namespace tempandwet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.zedGraphControl2 = new ZedGraph.ZedGraphControl();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 25);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(986, 637);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox5);
+            this.tabPage1.Controls.Add(this.groupBox4);
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Size = new System.Drawing.Size(978, 608);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "温度曲线";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Location = new System.Drawing.Point(7, 307);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(289, 296);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "环境气压";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(7, 6);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(289, 295);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "环境温湿度";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.zedGraphControl2);
+            this.groupBox3.Location = new System.Drawing.Point(302, 307);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(670, 296);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "温度控制实时曲线";
+            // 
+            // zedGraphControl2
+            // 
+            this.zedGraphControl2.Location = new System.Drawing.Point(7, 24);
+            this.zedGraphControl2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.zedGraphControl2.Name = "zedGraphControl2";
+            this.zedGraphControl2.ScrollGrace = 0D;
+            this.zedGraphControl2.ScrollMaxX = 0D;
+            this.zedGraphControl2.ScrollMaxY = 0D;
+            this.zedGraphControl2.ScrollMaxY2 = 0D;
+            this.zedGraphControl2.ScrollMinX = 0D;
+            this.zedGraphControl2.ScrollMinY = 0D;
+            this.zedGraphControl2.ScrollMinY2 = 0D;
+            this.zedGraphControl2.Size = new System.Drawing.Size(645, 256);
+            this.zedGraphControl2.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.zedGraphControl1);
+            this.groupBox2.Location = new System.Drawing.Point(302, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(670, 296);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "危险气体浓度实时曲线";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(7, 23);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(645, 255);
+            this.zedGraphControl1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox6);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage2.Size = new System.Drawing.Size(978, 608);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "运行状态";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tabControl1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(992, 666);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "实时数据";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.checkBox4);
+            this.groupBox6.Controls.Add(this.checkBox3);
+            this.groupBox6.Controls.Add(this.checkBox2);
+            this.groupBox6.Controls.Add(this.checkBox1);
+            this.groupBox6.Controls.Add(this.comboBox2);
+            this.groupBox6.Controls.Add(this.label1);
+            this.groupBox6.Controls.Add(this.comboBox1);
+            this.groupBox6.Controls.Add(this.button1);
+            this.groupBox6.Location = new System.Drawing.Point(7, 6);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(965, 100);
+            this.groupBox6.TabIndex = 0;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "groupBox6";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "端口号";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(106, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "波特率";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(318, 39);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(476, 43);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(37, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "a";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(519, 43);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(37, 19);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "b";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(563, 43);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(37, 19);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "c";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(607, 43);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(37, 19);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "d";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 677);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "温度湿度测量系统";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private ZedGraph.ZedGraphControl zedGraphControl2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ tempandwet/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using ZedGraph;
+
+
+namespace tempandwet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.zedbind();
+            timer1.Start();
+        }
+        PointPairList list1 = new PointPairList();
+        PointPairList list2 = new PointPairList();
+        PointPairList list3 = new PointPairList();
+        PointPairList list4 = new PointPairList();
+        double Average1(double d1,double d2,double b)
+        {
+            double O1=0.0;
+            O1 = 0.9 * d1 + (1 - 0.9) * d2;
+            O1 = O1 / (1 - b);
+            return O1;
+        }
+        double data1 = 0.0;
+        double data2 = 0.0;
+        double miao = 0;
+        double buf = 0.0;
+        double b = 0.9;
+        double o=0.0;
+        private void readdata()
+        {
+            try
+            {
+                miao = (double)new XDate(DateTime.Now.AddMilliseconds(-50));
+
+                Random rd = new Random();
+                buf = data1;
+
+                data1 = 20 + rd.Next(6, 9);
+                data2 = 80 + rd.Next(5, 16);
+                if (list1.Count > 50)
+                    list1.RemoveAt(0);
+                if (list2.Count > 50)
+                    list2.RemoveAt(0);
+                if (list3.Count > 50)
+                    list3.RemoveAt(0);
+                if (list4.Count > 50)
+                    list4.RemoveAt(0);
+                list1.Add(miao, data1);
+                list2.Add(miao, data2);
+                list3.Add(miao, data1);
+                list4.Add(miao, data1);
+                buf = o;
+                b = b * 0.9;
+                this.zedrefreshmethod();
+                Thread.Sleep(50);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+        }
+        private void zedbind()
+        {
+            //Y1
+            zedGraphControl1.GraphPane.YAxis.Title.Text = "浓度/ppm";
+            zedGraphControl1.GraphPane.YAxis.Scale.FontSpec.FontColor = Color.Black;
+            zedGraphControl1.GraphPane.YAxis.Title.FontSpec.FontColor = Color.Black;
+            zedGraphControl1.GraphPane.YAxis.Scale.Min =0;
+            zedGraphControl1.GraphPane.YAxis.Scale.Max = 255;
+            zedGraphControl1.GraphPane.YAxis.Scale.MajorStep = 15;
+            zedGraphControl1.GraphPane.YAxis.Scale.MinorStep = 5;
+            zedGraphControl1.GraphPane.YAxis.Scale.MinorStepAuto = true;
+            zedGraphControl1.GraphPane.YAxis.Scale.MajorStepAuto = true;
+            zedGraphControl1.GraphPane.YAxis.Title.FontSpec.Size = 20;  
+            //
+            //Y2
+            zedGraphControl2.GraphPane.YAxis.Title.Text = "温度/℃";
+            zedGraphControl2.GraphPane.YAxis.Scale.FontSpec.FontColor = Color.Black;
+            zedGraphControl2.GraphPane.YAxis.Title.FontSpec.FontColor = Color.Black;
+            zedGraphControl2.GraphPane.YAxis.Scale.Min = 25;
+            zedGraphControl2.GraphPane.YAxis.Scale.Max = 35;
+            zedGraphControl2.GraphPane.YAxis.Scale.MajorStep = 5;
+            zedGraphControl2.GraphPane.YAxis.Scale.MinorStep = 1;
+            zedGraphControl2.GraphPane.YAxis.Scale.MinorStepAuto = true;
+            zedGraphControl2.GraphPane.YAxis.Scale.MajorStepAuto = true;
+            zedGraphControl2.GraphPane.YAxis.Title.FontSpec.Size = 20;
+            //
+            zedGraphControl1.GraphPane.XAxis.Title.Text = "时间/mm:ss";
+            zedGraphControl2.GraphPane.XAxis.Title.Text = "时间/mm:ss";
+            
+            zedGraphControl1.GraphPane.XAxis.Type = ZedGraph.AxisType.DateAsOrdinal;
+            zedGraphControl1.GraphPane.Title.IsVisible=false;
+            zedGraphControl1.GraphPane.XAxis.Title.FontSpec.Size = 20;
+
+            zedGraphControl2.GraphPane.XAxis.Type = ZedGraph.AxisType.DateAsOrdinal;
+            zedGraphControl2.GraphPane.Title.IsVisible=false;
+            zedGraphControl2.GraphPane.XAxis.Title.FontSpec.Size = 20;
+
+            LineItem l1=zedGraphControl1.GraphPane.AddCurve("CO", list1, Color.Blue);
+            LineItem l2=zedGraphControl1.GraphPane.AddCurve("CH4", list2, Color.Red);
+            LineItem l3=zedGraphControl2.GraphPane.AddCurve("Heat_temp", list3, Color.Green);
+
+        }
+        private delegate void ZedRefreshHandle();
+        private void zedrefreshmethod()
+        {
+            if (this.InvokeRequired)
+            {
+                ZedRefreshHandle zrh = new ZedRefreshHandle(zedrefreshmethod);
+                this.Invoke(zrh, new object[] { });
+            }
+            else
+            {
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Refresh();
+                zedGraphControl2.AxisChange();
+                zedGraphControl2.Refresh();
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            zedGraphControl1.Size = new Size(this.Size.Width - 162, (this.Size.Height - 115) / 2);
+            zedGraphControl2.Size = new Size(this.Size.Width - 162, (this.Size.Height - 115) / 2);
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                try
+                {
+                    serialPort1.Close();
+                }
+                catch { }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            readdata();
+        }
+    }
+}
